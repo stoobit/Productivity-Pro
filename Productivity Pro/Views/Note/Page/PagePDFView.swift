d643098b8ba89ee59b902b42ca83b2a4fef7dfcd
Till Brügmann <till.bruegmann@icloud.com>
2023-06-02 00:28:16 +0200

    PagePDFView

--- Productivity Pro/Views/Note/Page/PagePDFView.swift
+++ Productivity Pro/Views/Note/Page/PagePDFView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 import PDFKit
 
-struct PDFKitRepresentedView: UIViewRepresentable {
-    typealias UIViewType = PDFView
-
+struct PagePDFView: View {
+    
+    @Binding var page: Page
     @StateObject var toolManager: ToolManager
     
-    let data: Data
+    var body: some View {
+        PDFKitRepresentedView(
+            toolManager: toolManager,
+            backgroundData: page.backgroundMedia
+        )
+    }
+    
+    func getFrame() -> CGSize {
+        var frame: CGSize = .zero
+        
+        if page.isPortrait {
+            frame = CGSize(width: shortSide, height: longSide)
+        } else {
+            frame = CGSize(width: longSide, height: shortSide)
+        }
+        
+        return frame
+    }
+}
+
+struct PDFKitRepresentedView: UIViewRepresentable {
+
+    @StateObject var toolManager: ToolManager
+    let backgroundData: Data?
     
     func makeUIView(context: Context) -> PDFView {
         let pdfView = PDFView()
         
-        pdfView.document = PDFDocument(data: data)
+        if let data = backgroundData {
+            pdfView.document = PDFDocument(data: data)
+        }
+        
         pdfView.displayMode = .singlePage
         pdfView.isUserInteractionEnabled = false
         pdfView.pageShadowsEnabled = false
