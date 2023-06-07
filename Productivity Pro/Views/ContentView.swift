00952f2bbfd3a6ffc0234e850fc42f4316bce1b4
Till Brügmann <till.bruegmann@icloud.com>
2023-06-07 19:19:46 +0200

    Buying Text fi

--- Productivity Pro/Views/ContentView.swift
+++ Productivity Pro/Views/ContentView.swift
@@ -64,7 +64,7 @@ struct ContentView: View {
                 }
             },
             message: {
-                Text("By purchasing Productivity Pro you are helping us to improve the app and add more features allowing you to be even more productive.")
+                Text("By purchasing Productivity Pro, you help us improve the app and add more features, enabling you to be even more productive.")
             }
         )
         
