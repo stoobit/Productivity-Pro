077099165cbf968260f6f3172ebdfe35b4595e56
stoobit <till.bruegmann@icloud.com>
2023-07-15 15:57:51 +0200

    VisionOS Improvements

---
+++ Productivity Pro/RealityContentView.swift
@@ -0,0 +1,18 @@
+//
+//  RealityContentView.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 15.07.23.
+//
+
+import SwiftUI
+
+struct RealityContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RealityContentView()
+}
