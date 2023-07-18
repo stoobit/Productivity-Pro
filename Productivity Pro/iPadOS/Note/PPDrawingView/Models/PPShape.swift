5f2eddc4b086d178e9b21901b80bb8766abd2281
stoobit <till.bruegmann@icloud.com>
2023-07-19 00:42:25 +0200

    Drawing Basics finished

---
+++ Productivity Pro/iPadOS/Note/PPDrawingView/Models/PPShape.swift
@@ -0,0 +1,18 @@
+//
+//  PPShape.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 19.07.23.
+//
+
+import SwiftUI
+
+struct PPShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PPShape()
+}
