faf808f415e4e58740abfc1a7f1b82ed1ac1d052
stoobit <till.bruegmann@icloud.com>
2023-07-20 13:45:38 +0200

    PPDrawingBar

---
+++ Productivity Pro/iPadOS/Note/PPToolbar/PPMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  PPMenuView.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 20.07.23.
+//
+
+import SwiftUI
+
+struct PPMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PPMenuView()
+}
---
+++ Productivity Pro/iPadOS/Note/PPToolbar/PPSizeButton.swift
@@ -0,0 +1,18 @@
+//
+//  PPSizeButton.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 20.07.23.
+//
+
+import SwiftUI
+
+struct PPSizeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PPSizeButton()
+}
