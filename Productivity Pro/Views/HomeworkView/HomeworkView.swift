e591962802c3cdc955e27d557e0cf27543de3c42
stoobit <till.bruegmann@icloud.com>
2023-09-12 15:33:35 +0200

    HomeworkView

---
+++ Productivity Pro/Views/HomeworkView/HomeworkView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeworkView.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 12.09.23.
+//
+
+import SwiftUI
+
+struct HomeworkView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(UIColor.systemGroupedBackground)
+                    .ignoresSafeArea(.all)
+                
+                VStack {
+                    
+                }
+            }
+            .navigationTitle("Hausaufgaben")
+        }
+    }
+}
+
+#Preview {
+    HomeworkView()
+}
