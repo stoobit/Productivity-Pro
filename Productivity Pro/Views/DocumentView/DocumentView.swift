0270b923993a41ce9b60c831d5088cafc01dded4
stoobit <till.bruegmann@icloud.com>
2023-09-14 14:27:03 +0200

    opt

--- Productivity Pro/Views/DocumentView/DocumentView.swift
+++ Productivity Pro/Views/DocumentView/DocumentView.swift
@@ -19,31 +19,33 @@ struct DocumentView: View {
     
     var url: URL
     var body: some View {
-        DocumentTypeContainerView(
-            document: $document,
-            subviewManager: subviewManager,
-            toolManager: toolManager,
-            url: url
-        )
-        .disabled(toolManager.showProgress)
-        .overlay {
-            if toolManager.showProgress {
-                ProgressView("Processing...")
-                    .progressViewStyle(.circular)
-                    .tint(.accentColor)
-                    .frame(width: 175, height: 100)
-                    .background(.thickMaterial)
-                    .cornerRadius(13, antialiased: true)
-            }
-        }
-        .sheet(isPresented: $subviewManager.sharePDFSheet) {
-            ShareSheet(
-                showProgress: $toolManager.showProgress,
+        NavigationStack {
+            DocumentTypeContainerView(
+                document: $document,
                 subviewManager: subviewManager,
                 toolManager: toolManager,
-                document: $document,
-                type: .pdf
+                url: url
             )
+            .disabled(toolManager.showProgress)
+            .overlay {
+                if toolManager.showProgress {
+                    ProgressView("Processing...")
+                        .progressViewStyle(.circular)
+                        .tint(.accentColor)
+                        .frame(width: 175, height: 100)
+                        .background(.thickMaterial)
+                        .cornerRadius(13, antialiased: true)
+                }
+            }
+            .sheet(isPresented: $subviewManager.sharePDFSheet) {
+                ShareSheet(
+                    showProgress: $toolManager.showProgress,
+                    subviewManager: subviewManager,
+                    toolManager: toolManager,
+                    document: $document,
+                    type: .pdf
+                )
+            }
         }
         
     }
