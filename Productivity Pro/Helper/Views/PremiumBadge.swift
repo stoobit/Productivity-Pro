0d5ee2ecf24290648f108d4601674db1f023f983
stoobit <till.bruegmann@icloud.com>
2023-09-24 19:58:29 +0200

    notifications

---
+++ Productivity Pro/Helper/Views/PremiumBadge.swift
@@ -0,0 +1,18 @@
+//
+//  PremiumBadge.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 24.09.23.
+//
+
+import SwiftUI
+
+struct PremiumBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PremiumBadge()
+}
