ee6a05bafdbfa980b84ba8d3ae6035febaaaf3f6
stoobit <till.bruegmann@icloud.com>
2023-09-26 19:19:55 +0200

    Backup

---
+++ Productivity Pro/Views/DeskView/BackupSettings.swift
@@ -0,0 +1,18 @@
+//
+//  BackupSettings.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 26.09.23.
+//
+
+import SwiftUI
+
+struct BackupSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackupSettings()
+}
