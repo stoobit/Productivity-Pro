f6cf50a5555e98f467c42f25095aecfa7a5a4d89
stoobit <till.bruegmann@icloud.com>
2023-10-28 22:57:06 +0200

    PremiumView

---
+++ Productivity Pro/Helper/Views/AdaptiveView.swift
@@ -0,0 +1,33 @@
+//
+//  AdaptiveView.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 28.10.23.
+//
+
+import SwiftUI
+
+struct AdaptiveStack<Content: View>: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+    let horizontalAlignment: HorizontalAlignment
+    let verticalAlignment: VerticalAlignment
+    let spacing: CGFloat?
+    let content: () -> Content
+
+    init(horizontalAlignment: HorizontalAlignment = .center, verticalAlignment: VerticalAlignment = .center, spacing: CGFloat? = nil, @ViewBuilder content: @escaping () -> Content) {
+        self.horizontalAlignment = horizontalAlignment
+        self.verticalAlignment = verticalAlignment
+        self.spacing = spacing
+        self.content = content
+    }
+
+    var body: some View {
+        Group {
+            if sizeClass == .compact {
+                VStack(alignment: horizontalAlignment, spacing: spacing, content: content)
+            } else {
+                HStack(alignment: verticalAlignment, spacing: spacing, content: content)
+            }
+        }
+    }
+}
--- Productivity Pro/Views/DeskView/PremiumView.swift
+++ Productivity Pro/Views/DeskView/PremiumView.swift
@@ -6,19 +6,81 @@
 //
 
 import SwiftUI
+import Glassfy
 
 struct PremiumView: View {
+    @AppStorage("ppisunlocked") var isSubscribed: Bool = false
+    
+    @Environment(\.horizontalSizeClass) var hsc
     @EnvironmentObject var iapModel: IAPViewModel
     
-    @AppStorage("ppisunlocked")
-    var isSubscribed: Bool = false
+    @State var type = "com.stoobit.productivity.monthly"
     
     var body: some View {
-        Text("\(iapModel.products.count)")
+        VStack {
+            Spacer()
+            
+            ForEach(iapModel.products, id: \.self) { product in
+                ProductView(product: product)
+            }
+            
+            Spacer()
+            FooterView()
+                .padding(30)
+        }
+        .navigationTitle("Premium")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Wiederherstellen", systemImage: "purchased") {
+                    
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder 
+    func ProductView(product: Glassfy.Sku) -> some View {
+        Button(action: { type = product.product.productIdentifier }) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(.ultraThickMaterial)
+                
+                HStack {
+                    VStack {
+                        Text("Monatliches Abo")
+                            .font(.title3.bold())
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
+                    
+                }
+                .padding(13)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 30)
+            .padding(.vertical, 10)
+        }
+    }
+    
+    @ViewBuilder 
+    func FooterView() -> some View {
+        Button(action: {  }) {
+            Text("Premium abonnieren")
+                .font(.title3.bold())
+                .foregroundStyle(Color.white)
+        }
+        .padding(.vertical, 14)
+        .padding(.horizontal, hsc == .regular ? 60 : 45)
+        .background(Color.blue)
+        .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
     }
 }
 
 #Preview {
-    PremiumView()
-        .environmentObject(IAPViewModel())
+    NavigationStack {
+        PremiumView()
+            .environmentObject(IAPViewModel())
+    }
 }
