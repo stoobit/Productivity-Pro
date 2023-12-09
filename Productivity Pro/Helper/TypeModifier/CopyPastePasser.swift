26e38c7a963720527f701219fb2e3aaddcf15146
stoobit <till.bruegmann@icloud.com>
2023-12-09 11:43:31 +0100

    CPPasser

---
+++ Productivity Pro/Helper/TypeModifier/CopyPastePasser.swift
@@ -0,0 +1,16 @@
+//
+//  CopyPastePasser.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 09.12.23.
+//
+
+import Foundation
+
+enum CopyPastePasser: Equatable {
+    case copy
+    case duplicate
+    case paste
+    case cut
+    case delete
+}
