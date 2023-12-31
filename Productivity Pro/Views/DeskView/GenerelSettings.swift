2537aa5df5e2785dece0540369ec887e0d454a43
stoobit <till.bruegmann@icloud.com>
2023-12-31 21:54:28 +0100

    default font size setter

--- Productivity Pro/Views/DeskView/GenerelSettings.swift
+++ Productivity Pro/Views/DeskView/GenerelSettings.swift
@@ -5,14 +5,13 @@
 //  Created by Till Brügmann on 10.09.23.
 //
 
+import PPDoubleKeyboard
 import SwiftUI
 
 struct GenerelSettings: View {
-    
     @Environment(\.horizontalSizeClass) var hsc
     
     @AppStorage("pprole") var role: Role = .individual
-    
     @AppStorage("notificationTime")
     var notificationTime: Date = Calendar.current.date(
         bySettingHour: 15, minute: 30, second: 00, of: Date()
@@ -31,10 +30,11 @@ struct GenerelSettings: View {
     @State private var sizeSetter: Double = 12
     @State private var cbSetter: Int = 0
     
+    @State var showSizePicker: Bool = false
+    
     var body: some View {
         NavigationStack {
             Form {
-                
                 Section("Rolle") {
                     Picker("", selection: $role) {
                         Text("Individuell")
@@ -73,22 +73,19 @@ struct GenerelSettings: View {
                         defaultFont = fontSetter
                     }
                     
-                    TextField("Schriftgröße", value: $sizeSetter, format: .number)
-                        .keyboardType(.decimalPad)
-                        .frame(height: 30)
-                        .onChange(of: sizeSetter) {
-                            if sizeSetter < 1 {
-                                sizeSetter = 1
-                                defaultFontSize = sizeSetter
-                            } else if sizeSetter > 1000 {
-                                sizeSetter = 1000
-                                defaultFontSize = sizeSetter
-                            } else {
-                                defaultFontSize = sizeSetter
-                            }
+                    HStack {
+                        Text("Schriftgröße")
+                        Spacer()
+                        Button(String(defaultFontSize)) {
+                            showSizePicker.toggle()
                         }
+                        .ppDoubleKeyboard(isPresented: $showSizePicker, value: $sizeSetter)
+                        .onChange(of: sizeSetter) {
+                            defaultFontSize = sizeSetter
+                        }
+                    }
+                    .frame(height: 30)
                 }
-                
             }
             .environment(\.defaultMinListRowHeight, 10)
             .navigationTitle("Allgemein")
@@ -98,7 +95,6 @@ struct GenerelSettings: View {
             }
         }
     }
-    
 }
 
 #Preview {
