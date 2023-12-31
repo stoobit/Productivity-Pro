2e34cd37a36eed8645e52d6d06ad192604d66636
stoobit <till.bruegmann@icloud.com>
2023-12-31 22:19:37 +0100

    clipboard methods preperations

--- Productivity Pro/Views/Note/ClipboardControl/ClipboardMethods.swift
+++ Productivity Pro/Views/Note/ClipboardControl/ClipboardMethods.swift
@@ -25,18 +25,88 @@ extension ClipboardControl {
         
         exportable.x += 50
         exportable.y += 50
+        
+        let duplicate = ImportManager().ppImport(item: exportable)
+        toolManager.activePage?.items?.append(duplicate)
+        toolManager.activeItem = duplicate
     }
     
     func copy() {
-//        let jsonData = try? JSONEncoder().encode(toolManager.activeItem)
-//        if let data = jsonData {
-//            UIPasteboard.general.setData(
-//                data, forPasteboardType: "productivity pro"
-//            )
-//        }
+        guard let item = toolManager.activeItem else { return }
+        
+        let exportable = ExportManager().export(item: item)
+        let jsonData = try? JSONEncoder().encode(exportable)
+        
+        if let data = jsonData {
+            UIPasteboard.general.setData(
+                data, forPasteboardType: "productivityproitem"
+            )
+        }
     }
     
-    func paste() {}
+    func paste() {
+        if UIPasteboard.general.hasImages {
+            addImage()
+        } else if UIPasteboard.general.hasStrings {
+            addTextField()
+        } else if UIPasteboard.general.contains(pasteboardTypes: ["productivityproitem"]) {
+            addItem()
+        }
+    }
     
-    func cut() {}
+    func addImage() {
+        toolManager.showProgress = true
+        Task {
+            await MainActor.run {
+                addImage()
+                
+                toolManager.showProgress = false
+                toolManager.copyPastePasser = .none
+            }
+        }
+    }
+    
+    func addTextField() {}
+    
+    func addItem() {
+        Task {
+            await MainActor.run {
+                do {
+                    let jsonData = UIPasteboard.general
+                        .data(forPasteboardType: "productivityproitem")
+                    
+                    if let data = jsonData {
+                        var pasteItem: ItemModel = try JSONDecoder().decode(
+                            ItemModel.self, from: data
+                        )
+                        
+                        toolManager.showProgress = true
+                        
+                        pasteItem.id = UUID()
+                        
+                        pasteItem.x = toolManager.offset.size.width * (1/toolManager.scale) + pasteItem.width/2 + 40
+                        
+                        pasteItem.y = toolManager.scrollOffset.size.height * (1/toolManager.zoomScale) + pasteItem.height/2 + 40
+                        
+                        pasteItem.isLocked = false
+                        
+                        document.document.note.pages[
+                            toolManager.selectedPage
+                        ].items.append(pasteItem)
+                        
+                        toolManager.selectedItem = pasteItem
+                    }
+                    
+                } catch { print("fail") }
+                
+                toolManager.showProgress = false
+                toolManager.copyPastePasser = .none
+            }
+        }
+    }
+    
+    func cut() {
+        copy()
+        delete()
+    }
 }
