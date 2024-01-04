b8a6a9e5852effc81935b15c56af75c4594d26f0
stoobit <till.bruegmann@icloud.com>
2024-01-05 00:15:28 +0100

    AppClips

---
+++ Productivity Preview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Productivity Preview
+//
+//  Created by Till Brügmann on 04.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Productivity Preview/Productivity_PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Productivity_PreviewApp.swift
+//  Productivity Preview
+//
+//  Created by Till Brügmann on 04.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Productivity_PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
