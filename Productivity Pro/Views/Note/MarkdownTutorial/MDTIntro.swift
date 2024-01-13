868b8b2fd38e0b182568d7ba519fc0c21215a611
stoobit <till.bruegmann@icloud.com>
2024-01-13 20:55:41 +0100

    .pronote export

---
+++ Productivity Pro/Views/Note/MarkdownTutorial/MDTIntro.swift
@@ -0,0 +1,18 @@
+//
+//  MDTIntro.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 13.01.24.
+//
+
+import SwiftUI
+
+struct MDTIntro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MDTIntro()
+}
