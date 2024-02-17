8ed249f00d65d813bad11dd1b3c974015882cb19
stoobit <till.bruegmann@icloud.com>
2024-02-17 22:01:02 +0100

    pdf export

---
+++ Productivity Pro/Views/Note/Page/BackgroundViews/RenderedPDF.swift
@@ -0,0 +1,18 @@
+//
+//  RenderedPDF.swift
+//  Productivity Pro
+//
+//  Created by Till Brügmann on 17.02.24.
+//
+
+import SwiftUI
+
+struct RenderedPDF: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RenderedPDF()
+}
